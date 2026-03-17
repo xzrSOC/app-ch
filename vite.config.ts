ccaa70b371334cb4b766812d54893637e516736b
xzrSOC <patrikvasovic041@gmail.com>
2026-03-17 12:16:38 +0100

    a

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/app-ch/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/app-ch/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
